4112c16404de83ddee9408c960916fc38f9ee1a5
carloscondit <bazki.kz@mail.ru>
2021-02-03 18:24:58 +0300

    Перенес новогоднее условие в функции приглашения. Добавил регионы.

--- Profile/profile.ps1
+++ Profile/profile.ps1
@@ -1,4 +1,5 @@
-﻿# Устанавливаем корневую папку            
+﻿#region Preferences
+# Устанавливаем корневую папку            
 $Root = "C:\Distr"             
             
 # Переходим в каталог со скриптами           
@@ -21,6 +22,29 @@ foreach ($sid in [Security.Principal.WindowsIdentity]::GetCurrent().Groups) {
   Write-Verbose "Добавляем в переменную среду Path каталог ""$_"""            
   $env:path += ";$_"            
 }
+
+# Изменяем цвета текста, чтобы было легче читать.
+if ($IsCoreCLR) {
+  $esc = "`e"
+}
+else {
+  $esc = $([char]0x1b)
+}
+
+Set-PSReadLineOption -Colors @{
+  Parameter = "$esc[96m"
+  Operator  = "$esc[38;5;47m"
+  Comment   = "$esc[92m"
+  String    = "$esc[38;5;51m"
+}
+
+# Если это не PowerShell ISE и версия модуля PSReadLine выше или равна 2.2.0, то добавляем функцию автодополения на основе истории команд. 
+if ((!$psISE) -and ((Get-Module PSReadline).version -ge '2.2.0')) {
+  Set-PSReadLineOption -PredictionSource History -Colors @{ InlinePrediction = "$esc[38;2;47;112;4m" }
+}
+#endregion
+
+#region Functions
 # Добавляем функции sudo и resudo, чтобы можно было запускать/перезапускать команды с правами администратора одной командой не выходя из консоли
 # Источник http://www.outsidethebox.ms/20532/
 
@@ -41,46 +65,24 @@ function sudo (
   Start-Process -FilePath powershell -ArgumentList $cmdline -Verb runas
 }
 
-# Изменяем цвета текста, чтобы было легче читать.
-if ($IsCoreCLR) {
-  $esc = "`e"
-}
-else {
-  $esc = $([char]0x1b)
-}
- 
-Set-PSReadLineOption -Colors @{
-  Parameter = "$esc[96m"
-  Operator  = "$esc[38;5;47m"
-  Comment   = "$esc[92m"
-  String    = "$esc[38;5;51m"
-}
-
-# Если это не PowerShell ISE и версия модуля PSReadLine выше или равна 2.2.0, то добавляем функцию автодополения на основе истории команд. 
-if ((!$psISE) -and ((Get-Module PSReadline).version -ge '2.2.0')) {
-  Set-PSReadLineOption -PredictionSource History -Colors @{ InlinePrediction = "$esc[38;2;47;112;4m" }
-}
-
 # Добавляем функцию приглашения, чтобы было видно с какими правами запущен процесс PowerShell.
 # А также строчку оповедение о приближени Нового Года.
 # Подробнее про оповещение тут: https://jdhitsolutions.com/blog/powershell/7956/friday-fun-a-powershell-christmas-prompt/
 # А про изменение самой строки приглашения: https://xaegr.wordpress.com/2009/06/01/myprofile/
 
-if ((Get-Date).Month -eq 12 -AND (Get-Date).Day -gt 11) {
-  #При необходимости добавляем скрипт с функциями конвертирования эмоджи
-  # Подробнее тут: https://gist.github.com/jdhitsolutions/31e20c58645b59e42725f0aac0297b6f
-  #. C:\Distr\Scripts\Fun\PSEmoji.ps1
-  
-  Function Prompt {
-    #Получаем следующий год
-    $year = ((get-date).AddYears(1)).Year
 
-    #Получаем количество времени до нового года
-    $time = [datetime]"1 January $year" - (Get-Date)
-    #Превращаем это время в строку без милисекунд
-    $timestring = $time.ToString("dd' дней и 'hh':'mm':'ss")
-    
+Function Prompt {
+  if ((Get-Date).Month -eq 12 -AND (Get-Date).Day -gt 11) {
     if ($env:wt_Session -OR ($host.name -match "studio")) {
+      #При необходимости добавляем скрипт с функциями конвертирования эмоджи
+      # Подробнее тут: https://gist.github.com/jdhitsolutions/31e20c58645b59e42725f0aac0297b6f
+      #. C:\Distr\Scripts\Fun\PSEmoji.ps1
+      #Получаем следующий год
+      $year = ((get-date).AddYears(1)).Year
+      #Получаем количество времени до нового года
+      $time = [datetime]"1 January $year" - (Get-Date)
+      #Превращаем это время в строку без милисекунд
+      $timestring = $time.ToString("dd' дней и 'hh':'mm':'ss")
       #Получаем рандомную строку из декоративных символов
       #Можно указать конкретный эмоджи или сконвертировать из значений, если доступна функция ConvertTo-Emoji
       # $Snow = ""
@@ -108,31 +110,31 @@ if ((Get-Date).Month -eq 12 -AND (Get-Date).Day -gt 11) {
       $colorText = $text.tocharArray() | ForEach-Object {
         $i = Get-Random -Minimum 1 -Maximum 50
         switch ($i) {
-          { $i -le 50 -AND $i -ge 45 } { $seq = "$([char]0x1b)[1;5;38;5;199m" }
-          { $i -le 45 -AND $i -ge 40 } { $seq = "$([char]0x1b)[1;5;38;11;199m" }
-          { $i -le 40 -AND $i -ge 30 } { $seq = "$([char]0x1b)[1;38;5;50m" }
-          { $i -le 20 -and $i -gt 15 } { $seq = "$([char]0x1b)[1;5;38;5;1m" }
-          { $i -le 16 -and $i -gt 10 } { $seq = "$([char]0x1b)[1;38;5;47m" }
-          { $i -le 10 -and $i -gt 5 } { $seq = "$([char]0x1b)[1;5;38;5;2m" }
-          default { $seq = "$([char]0x1b)[1;37m" }
+          { $i -le 50 -AND $i -ge 45 } { $seq = "$esc[1;5;38;5;199m" }
+          { $i -le 45 -AND $i -ge 40 } { $seq = "$esc[1;5;38;11;199m" }
+          { $i -le 40 -AND $i -ge 30 } { $seq = "$esc[1;38;5;50m" }
+          { $i -le 20 -and $i -gt 15 } { $seq = "$esc[1;5;38;5;1m" }
+          { $i -le 16 -and $i -gt 10 } { $seq = "$esc[1;38;5;47m" }
+          { $i -le 10 -and $i -gt 5 } { $seq = "$esc[1;5;38;5;2m" }
+          default { $seq = "$esc[1;37m" }
         }
-        "$seq$_$([char]0x1b)[0m"
+        "$seq$_$esc[0m"
       } #foreach
       
       #Пишем эту строку в консоль на отдельную линию
       Write-Host "$front $($colortext -join '') $back" #-NoNewline #-foregroundcolor $color
     } #if Host is Windows Terminal or VS code
+  } #If December 
     
-    [Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem).ProviderPath            
-    $path = (Get-Location).path -replace '^(.*?[^:]:\\).+(\\.+?)$', ('$1' + [char]8230 + '$2') -replace '^.+?::' -replace '^(\\\\.+?\\).+(\\.+?)$', ('$1' + [char]8230 + '$2')            
-    $id = ([int](Get-History -Count 1).Id) + 1            
-    $prefix = "[PS <$id> "            
-    if ($NestedPromptLevel) { $prefix += "($NestedPromptLevel) " }            
-    if ($isElevated) { $Color = "Red" } else { $Color = "White" }            
-    write-host $prefix -ForegroundColor $Color -NonewLine            
-    write-host ($path) -foregroundcolor "Gray" -NonewLine            
-    write-host "]" -ForegroundColor $Color -NonewLine            
-    " "
-  } #end function
-  
-} #If December 
+  [Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem).ProviderPath            
+  $path = (Get-Location).path -replace '^(.*?[^:]:\\).+(\\.+?)$', ('$1' + [char]8230 + '$2') -replace '^.+?::' -replace '^(\\\\.+?\\).+(\\.+?)$', ('$1' + [char]8230 + '$2')            
+  $id = ([int](Get-History -Count 1).Id) + 1            
+  $prefix = "[PS <$id> "            
+  if ($NestedPromptLevel) { $prefix += "($NestedPromptLevel) " }            
+  if ($isElevated) { $Color = "Red" } else { $Color = "White" }            
+  write-host $prefix -ForegroundColor $Color -NonewLine            
+  write-host ($path) -foregroundcolor "Gray" -NonewLine            
+  write-host "]" -ForegroundColor $Color -NonewLine            
+  " "
+} #end function Prompt
+#endregion
